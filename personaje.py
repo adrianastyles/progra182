af8c983868f1b1a17a788f03aef6ceab331571be
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-02-20 15:28:47 -0600

    agregar método correr

--- personaje.py
+++ personaje.py
@@ -1,5 +1,14 @@
 class Personaje:
+    
     #atributos
     especie = "Humano"
     nombre = "Harry Styles"
     altura = 1.83
+    
+    #métodos
+    def correr(self, status):
+        if(status):
+            print("El personaje " + self.nombre + " está corriendo.")
+        else:
+            print("El personaje " + self.nombre + " se detuvo.")
+    
