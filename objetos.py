6748f38587297cf6dc178387122bd1bb1b093dd8
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-02-20 15:43:52 -0600

    agregar funcion str

--- objetos.py
+++ objetos.py
@@ -6,4 +6,4 @@ heroe = Personaje()
 #usamos atributos
 print("El personaje se llama " + heroe.nombre)
 print("Pertenece a la especie " + heroe.especie)
-print("Su altura es " + heroe.altura)
+print("Su altura es " + str(heroe.altura))
