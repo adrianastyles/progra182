4d6fb29c6578af22776e30441f4f8b7597065419
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-02-22 15:22:56 -0600

    hacer espacios

--- objetos.py
+++ objetos.py
@@ -1,13 +1,13 @@
 from personaje import *
 
-print ("SOLICITUD DE DATOS PARA EL HÉROE \n")
+print ("\nSOLICITUD DE DATOS PARA EL HÉROE \n")
 
 especieH = input("Escribe la especie del héroe: ")
 nombreH = input("Escribe el nombre del héroe: ")
 alturaH = float(input("Escribe la altura del héroe: "))
 recargaH = int(input("Ingresa las balas para el héroe: "))
 
-print ("SOLICITUD DE DATOS PARA EL VILLANO \n")
+print ("\nSOLICITUD DE DATOS PARA EL VILLANO \n")
 
 especieV = input("Escribe la especie del villano: ")
 nombreV = input("Escribe el nombre del villano: ")
@@ -20,15 +20,15 @@ villano = Personaje(especieV, nombreV, alturaV)
 
 #usamos atributos héroe y villano
 
-print("HÉROE: \n")
+print("\nHÉROE: \n")
 print("El héroe se llama " + heroe.nombre)
 print("Pertenece a la especie " + heroe.especie)
 print("Su altura es " + str(heroe.altura))
 
-print("VILLANO: \n")
+print("\nVILLANO: \n")
 print("El villano se llama " + villano.nombre)
 print("Pertenece a la especie " + villano.especie)
-print("Su altura es " + str(villano.altura))
+print("Su altura es " + str(villano.altura) + "\n")
 
 #usar los métodos
 
