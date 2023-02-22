e9b0b090875b69110724eebc04f9886d99ca36c3
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-02-22 15:15:17 -0600

    solicitud de datos para los personajes

--- objetos.py
+++ objetos.py
@@ -1,5 +1,19 @@
 from personaje import *
 
+print ("SOLICITUD DE DATOS PARA EL HÉROE \n")
+
+especieH = input("Escribe la especie del héroe: ")
+nombreH = input("Escribe el nombre del héroe: ")
+alturaH = float(input("Escribe la altura del héroe: "))
+recargaH = int(input("Ingresa las balas para el héroe: "))
+
+print ("SOLICITUD DE DATOS PARA EL VILLANO \n")
+
+especieV = input("Escribe la especie del villano: ")
+nombreV = input("Escribe el nombre del villano: ")
+alturaV = float(input("Escribe la altura del villano: "))
+recargaV = int(input("Ingresa las balas para el villano: "))
+
 #instancia de la clase personaje
 heroe = Personaje()
 
