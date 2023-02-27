58219212cad8223958b889037c4504bbd2f22663
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-02-27 15:07:54 -0600

    agregar frames y colores

--- Tkinther/practica11.py
+++ Tkinther/practica11.py
@@ -5,4 +5,14 @@ ventana = Tk()
 ventana.title("Práctica 11")
 ventana.geometry("600x400")
 
+#2. Agregar Frames
+seccion1 = Frame(ventana, bg = "orange")
+seccion1.pack(expand = True, fill = 'both')
+
+seccion2 = Frame(ventana, bg = "pink")
+seccion2.pack(expand = True, fill = 'both')
+
+seccion3 = Frame(ventana, bg = "purple")
+seccion3.pack(expand = True, fill = 'both')
+
 ventana.mainloop()
