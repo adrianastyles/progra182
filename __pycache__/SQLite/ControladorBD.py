4efbca6a12fafeeb787319a0a631a4b674737b0f
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-03-24 23:35:52 -0600

    termino del programa

--- __pycache__/SQLite/ControladorBD.py
+++ __pycache__/SQLite/ControladorBD.py
@@ -1,3 +1,53 @@
 from tkinter import messagebox
 import sqlite3
 import bcrypt
+
+class controladorBD:
+    
+    def __init__ (self):
+        pass
+    
+    def conexionBD (self):
+        try:
+            conexion = sqlite3.connect ("C:/Users/mecat/OneDrive/Documentos/GitHub\POO182/__pycache__/SQLite/database15.db")
+            print ("conectado a la BD")
+            return conexion
+        
+        except sqlite3.OperationalError:
+            print ("no se pudo conectar")
+            
+    def GuardarUsuario (self, nom, cor, cont):
+        #llamar metodo conexion
+        conx = self.conexionBD()
+        
+        #validar vacíos
+        if (nom == "" or cor == "" or cont == ""):
+            messagebox.showwarning ("Warning", "Formulario incompleto")
+            conx.close()
+            
+        else:
+            #realizar insert a la base de datos
+            #preparar variables
+            cursor = conx.cursor()
+            conH = self.encriptarContra(cont)
+            datos = (nom, cor, conH)
+            sqlInsert = "insert into tbregistrados(nombre, correo, contraseña) values (?, ?, ?)"
+            
+            #ejecutar insert
+            cursor.execute(sqlInsert, datos)
+            conx.commit()
+            conx.close
+            messagebox.showinfo("Registro exitoso", "Usuario guardado")
+            
+    def encriptarContra (self, cont):
+        #preparamos contraseña y la sal para hash
+        conPlana = cont
+        conPlana = conPlana.encode() #convertir a byte
+        sal = bcrypt.gensalt()
+        
+        #encriptamos
+        conHa = bcrypt.hashpw(conPlana, sal)
+        print (conHa)
+        
+        #regresamos la contraseña encriptada
+        return conHa
--- __pycache__/SQLite/UsuariosCRUD.py
+++ __pycache__/SQLite/UsuariosCRUD.py
@@ -1,6 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from ControladorBD import *
+
+#Instancia: puente entre los dos archivos
+controlador = controladorBD()
+
+#metodo que usará mi objeto controlador para insertar
+def EjecutaInsert():
+    controlador.GuardarUsuario(varNom.get(), varCor.get(), varCont.get())
 
 Ventana = Tk()
 Ventana.title("CRUD de usuarios")
@@ -30,7 +38,7 @@ txtCor = Entry(pestana1, textvariable = varCor).pack()
 lblCont = Label(pestana1, text = "Contraseña:").pack()
 txtCont = Entry(pestana1, textvariable = varCont).pack()
 
-btnGuardar = Button(pestana1, text = "Guardar usuario").pack()
+btnGuardar = Button(pestana1, text = "Guardar usuario", command = EjecutaInsert).pack()
 
 
 panel.add(pestana1, text = 'Formulario de usuarios')
